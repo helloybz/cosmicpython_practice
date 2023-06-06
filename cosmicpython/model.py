80333f4d2f4c9cd06e52bcaddf883b9accc884d9
Youngbeom Choi <helloybz@gmail.com>
2023-06-06 16:59:05 +0900

    p.48

--- cosmicpython/model.py
+++ cosmicpython/model.py
@@ -13,15 +13,25 @@ class Batch:
     def __init__(self, ref: str, sku: str, qty: int, eta: date | None) -> None:
         self.ref = ref
         self.sku = sku
-        self.qty = qty
         self.eta = eta
+        self._purchased_qty = qty
+        self._allocated_lines: set[OrderLine] = set()
 
     def allocate(self, orderline: OrderLine) -> None:
-        if orderline.sku == self.sku:
-            self.qty -= orderline.qty
-        else:
-            raise ValueError("SKU not matched.")
+        if self.can_allocate(orderline):
+            self._allocated_lines.add(orderline)
+
+    def deallocate(self, orderline: OrderLine) -> None:
+        if orderline in self._allocated_lines:
+            self._allocated_lines.remove(orderline)
+
+    @property
+    def allocated_qty(self) -> int:
+        return sum(orderine.qty for orderine in self._allocated_lines)
 
     @property
     def available_quantity(self) -> int:
-        return self.qty
+        return self._purchased_qty - self.allocated_qty
+
+    def can_allocate(self, orderline: OrderLine) -> bool:
+        return self.sku == orderline.sku and self.available_quantity >= orderline.qty
